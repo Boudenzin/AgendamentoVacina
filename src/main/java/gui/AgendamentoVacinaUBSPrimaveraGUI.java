56b94eae7294b339b1ffd099efa85607f5ed4601
romildo.rodrigues <romildo-rrj@hotmail.com>
2024-09-25 14:42:22 -0300

    Mudança Parcial do GUI

--- src/main/java/gui/AgendamentoVacinaUBSPrimaveraGUI.java
+++ src/main/java/gui/AgendamentoVacinaUBSPrimaveraGUI.java
@@ -1,10 +1,27 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
+
+import controllers.*;
 public class AgendamentoVacinaUBSPrimaveraGUI extends JFrame {
 
+    ImageIcon zegotinha = new ImageIcon(".imgs/zegotinha.png");
+    JLabel linha1, linha2;
+    ImageIcon fotoPrincipal = zegotinha;
+    JMenuBar barraDeMeu = new JMenuBar();
+
     public AgendamentoVacinaUBSPrimaveraGUI() {
+
+        setTitle("Agendamento de Vacinas");
+        setSize(1820,720);
+
+        Dimension dimensaoTela = Toolkit.getDefaultToolkit().getScreenSize();
+        int larguraTela = dimensaoTela.width;
+        int alturaTela = dimensaoTela.height;
+
+
 
     }
 }
